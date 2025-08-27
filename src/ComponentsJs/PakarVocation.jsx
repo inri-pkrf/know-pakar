1618528655889b17012a9eab563a3540245995c5
elioral7 <elioral770@gmail.com>
2025-08-27 11:41:24 +0300

    הדף השני של הלומדה הושלם

--- src/ComponentsJs/PakarVocation.jsx
+++ src/ComponentsJs/PakarVocation.jsx
@@ -9,9 +9,16 @@ function PakarVocation() {
     { id: 2, text: 'קצין חיל ראשי', img: 'blue-man.png' }
   ];
 
+  const texts = [
+    { id: 0, text: "שירות ההתגוננות האזרחית כהגדרתו בחוק" },
+    { id: 1, text: "פיקוד מרחבי" },
+    { id: 2, text: "סמכות מקצועית ראשית בנושאי ההתגוננות האזרחית, החילוץ וההצלה" }
+  ]
+
   return (
     <div className="PakarVocation">
       <NavBar activeButton={0} className="navbar" />
+      <h1 id='title'>העבר עכבר מעל הכרטיסייה למידע נוסף</h1>
       <img 
         src={`${process.env.PUBLIC_URL}/assets/next-btn.png`} 
         alt="next-btn" 
@@ -20,28 +27,29 @@ function PakarVocation() {
       />
 
       <div id='tabs'>
-        {tabs.map(tab => (
-          <div key={tab.id} className="info-tab">
+        {tabs.map((tab, index) => (   
+        <div key={tab.id} className="info-tab">
             <div className="flip-card-inner">
-              {/* צד קדמי */}
-              <div className="card-front">
+            
+            {/* צד קדמי */}
+            <div className="card-front">
                 <div className="tab-text-wrapper">
-                  <p className='tab-text'>{tab.text}</p>
+                <p className='tab-text'>{tab.text}</p>
                 </div>
                 <img 
-                  src={`${process.env.PUBLIC_URL}/assets/${tab.img}`} 
-                  alt={tab.text} 
-                  className='man-pic'
+                src={`${process.env.PUBLIC_URL}/assets/${tab.img}`} 
+                alt={tab.text} 
+                className='man-pic'
                 />
-              </div>
-
-              {/* צד אחורי */}
-              <div className="card-back">
-                <h2>{tab.text}</h2>
-                <p>כאן ניתן להוסיף תוכן חדש או קומפוננטות נוספות</p>
-              </div>
             </div>
-          </div>
+
+            {/* צד אחורי */}
+            <div className="card-back">
+                <div id='definition'>הגדרה:</div>
+                <p>{texts[index].text}</p> 
+            </div>
+            </div>
+        </div>
         ))}
       </div>
     </div>
